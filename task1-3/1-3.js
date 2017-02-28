4365f4a07f780f5e0edf79eb1e821c0b5122e866
cooper <iohion@qq.com>
2017-02-28 23:38:12 +0800

    done task1-4

--- task1-3/1-3.js
+++ task1-3/1-3.js
@@ -1,6 +1,9 @@
     window.onload = function(){
         var bar = document.getElementsByClassName("bar")[0];
         var menu = document.getElementsByClassName("menu")[0];
+        var li = document.getElementsByTagName("li");
+        var list = Array.prototype.slice.call(li,0); //类数组转成数组
+
         bar.oncontextmenu = function(e){
             var e = e || window.event;
             if (e.stopPropagation){
@@ -27,7 +30,6 @@
             var offsetHeight = menu.offsetHeight;
             //alert(offsetHeight);
             //alert(browserWidth/2 + 151 - offsetWidth);
-            //条件写死了，后面要改进
             if (x > (browserWidth/2 + 151 - offsetWidth) && y > 92) {
                 menu.style.left = (x-offsetWidth) + "px";
                 menu.style.top = (y-offsetHeight) + "px";
@@ -42,10 +44,19 @@
                 menu.style.top = y + "px";
             }
         }
+
         window.onclick = function(e){
             menu.style.display = 'none';
         }
+
         window.oncontextmenu = function(e){
             menu.style.display = 'none';
         }
+        //遍历数组
+        list.forEach(function(item,index,arr){
+            li[index].onclick = function(event){
+                alert(this.innerHTML);
+                event.stopPropagation();
+            }
+        })
     }
